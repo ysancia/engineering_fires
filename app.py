62659afa191ab047e466706103a957924fc67f49
ysancia <ysancia@users.noreply.github.com>
2022-07-12 16:09:36 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -100,41 +100,30 @@ if new_input.select("rain") != 0.00:
 
 	results = pd.DataFrame(list(zip(labels,probs)),columns=["Class of Fire","Probability"])
 
-	st.text(
-		"Class A - one-fourth acre or less \n"
+	st.markdown("# Classes of Fire")
+	st.markdown(
+		"**Class A**: one-fourth acre or less \n"
 		)
-	st.text(
-		"Class B - more than one-fourth acre, but less than 10 acres \n"
+	st.markdown(
+		"**Class B** - more than one-fourth acre, but less than 10 acres \n"
 		)
-	st.text(
-		"Class C - 10 acres or more, but less than 100 acres \n"
+	st.markdown(
+		"**Class C** - 10 acres or more, but less than 100 acres \n"
 		)
-	st.text(
-		"Class D - 100 acres or more, but less than 300 acres \n"
+	st.markdown(
+		"**Class D** - 100 acres or more, but less than 300 acres \n"
 		)
-	st.text(
-		"Class E - 300 acres or more, but less than 1,000 acres \n"
+	st.markdown(
+		"**Class E** - 300 acres or more, but less than 1,000 acres \n"
 		)
-	st.text(
-		"Class F - 1,000 acres or more, but less than 5,000 acres \n"
+	st.markdown(
+		"**Class F** - 1,000 acres or more, but less than 5,000 acres \n"
 		)
-	st.text(
-		"Class G - 5,000 acres or more \n"
+	st.markdown(
+		"**Class G** - 5,000 acres or more \n"
 		)
 
 
 	st.markdown("# Predictions")
 	st.dataframe(results)
 
-
-
-
-
-	st.write(max_val,max_index)	
-	st.write(proba[0][0])
-	#for row in prob:
-	#	st.write(row[0])
-
-	st.write("high",high)
-	st.write("low",low)
-	st.write("rain",rain)
