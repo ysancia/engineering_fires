52c6c0fcde465596ceb50868809ed1d216446924
ysancia <ysancia@users.noreply.github.com>
2022-07-12 12:48:37 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+
+import streamlit as st
+import pandas as pd
+import matplotlib as plt
+import pyspark
+
+from pyspark.sql import SparkSession
+
+from pyspark.ml.classification import LogisticRegressionModel
+
+spark = (pyspark.sql.SparkSession.builder.getOrCreate())
+model = LogisticRegressionModel.load("./lr2")
+
+df = spark.read.load("processed_combined.parquet")
+train_df, test_df = df.randomSplit([0.7,0.3],seed=25)
+
+
+predictions = model.transform(test_df).cache()
+evaluate_acc = MulticlassClassificationEvaluator(labelCol="size_index",
+											predictionCol="Prediction",
+											metricName="accuracy")
+accuracy = evaluate_acc.evaluate(predictions)
+evaluate_r2 = MulticlassClassificationEvaluator(labelCol="size_index",
+											predictionCol="Prediction",
+											metricName="r2")
+r2 = evaluate_r2(predictions)
+evaluate_rmse = MulticlassClassificationEvaluator(labelCol="size_index",
+											predictionCol="Prediction",
+											metricName="accuracy")
+rmse = evaluate_rmse(predictions)
+
+preds = [int(row["prediction"]) for row in predictions.select("Prediction").collect()]
+actual = [int(row["size_index"]) for row in predictions.select("size_index").collect()]
+
+fig, ax = plt.subplots()
+ax.scatter(actual, preds, color='b', s=60, alpha=0.1)
+plt.plot([5,250], [5,250], color='r')
+plt.xlim([0, 260])
+plt.ylim([0, 260])
+ax.set_xlabel('Actual')
+ax.set_ylabel('Predicted')
+ax.set_title('Actual vs Predicted Prices',fontsize=20)
+col5.pyplot(fig)
