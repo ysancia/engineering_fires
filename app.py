f6faad73fd022d448ac2a831cfebb4e474470886
ysancia <ysancia@users.noreply.github.com>
2022-07-12 14:04:34 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import streamlit as st
 import pandas as pd
 from matplotlib import pyplot as plt
 import pyspark
+import numpy as np
 
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+
+from pyspark.ml.feature import VectorAssembler, MinMaxScaler
 
 conf = SparkConf().setAppName("model").setMaster("local")
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
@@ -27,26 +30,42 @@ evaluate_acc = MulticlassClassificationEvaluator(labelCol="size_index",
 											predictionCol="Prediction",
 											metricName="accuracy")
 accuracy = evaluate_acc.evaluate(predictions)
-#evaluate_r2 = MulticlassClassificationEvaluator(labelCol="size_index",
-											#predictionCol="Prediction",
-											#metricName="r2")
-#r2 = evaluate_r2.evaluate(predictions)
-#evaluate_rmse = MulticlassClassificationEvaluator(labelCol="size_index",
-											#predictionCol="Prediction",
-											#metricName="accuracy")
-#rmse = evaluate_rmse.evaluate(predictions)
 
 preds = [int(row["Prediction"]) for row in predictions.select("Prediction").collect()]
 actual = [int(row["size_index"]) for row in predictions.select("size_index").collect()]
-
+st.write("Model loaded")
+st.write("Model accuracy: ")
+st.write(accuracy)
 
 
 fig, ax = plt.subplots()
 ax.scatter(actual, preds, color='b', s=60, alpha=0.1)
 #plt.plot([0,7], [0,7], color='r')
+
 ax.set_xlabel('Actual')
 ax.set_ylabel('Predicted')
 ax.set_title('Actual vs Predicted category',fontsize=20)
-st.pyplot(fig)
+#st.pyplot(fig)
 
+features2 = ["LAT","LON","maxT","minT","precip"]
 
+lat = st.number_input("Enter your Latitude: ", min_value=None, max_value=None)
+lon = st.number_input("Enter your Longitude: ", min_value=None, max_value=None)
+high = st.number_input("Enter your daily high temperature (in F):  ", min_value=None, max_value=None)
+low = st.number_input("Enter your daily low temperature (in F): ", min_value=None, max_value=None)
+rain = st.number_input("Enter your precipitation (in inches): ", min_value=None, max_value=None)
+
+lat = np.round(lat,decimals=1)
+lon = np.round(lon,decimals=1)
+high = (high - 32) * 5/9
+low = (low - 32) * 5/9
+rain = (rain * 25.4) * 10
+
+new_input = [lat,lon,high,low,rain]
+
+va = VectorAssembler(inputCols=features2, outputCol = "features")
+
+new_input = va.transform(new_input)
+
+new_pred = model.transform(new_input)
+st.show(new_pred)
